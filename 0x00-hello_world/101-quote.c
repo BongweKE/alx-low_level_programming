b74c178b1f5cd36466e94e6f1191c48cfb26a45d
BongweKE <bongwe.obi@gmail.com>
2022-07-02 11:47:24 +0000

    0x00. C - Hello, World: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Print a line without using printf or puts
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
