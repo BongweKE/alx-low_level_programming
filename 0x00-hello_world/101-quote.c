5664adac66a47a97d5be1bf430a2a204d261ffb9
BongweKE <bongwe.obi@gmail.com>
2022-07-03 18:47:46 +0000

    0x01. C - Variables, if, else, while: 0. Positive anything is better than negative nothing v0.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 
 /**
- * main - Entry Point
- * Description: Print a line without using printf or puts
+ * main - Print a line without using printf or puts
  *
  * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char buf[20];
+	size_t nbytes;
+	ssize_t bytes_written;
+	int fd;
+
+	strcpy(buf,
+	       "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	nbytes = strlen(buf);
+	bytes_written = write(fd, buf, nbytes);
 	return (1);
 }
