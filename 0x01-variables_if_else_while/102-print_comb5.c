c8cd4f73faf2a66a1fc0f899dab87454a7569b09
BongweKE <bongwe.obi@gmail.com>
2022-07-04 21:56:44 +0000

    0x01. C - Variables, if, else, while: 102-print_comb5 v0.1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+
+/**
+ * i_print - output the characters in the given criteria
+ * @k: an array of two chars masked as short int to start the
+ * count. k represent a number e.g 23 is k0=2 and k1=3
+ *
+ * Description: Given k = 23 create i0=2 and i1=4 from which to
+ * print all numbers from 24 to 99 adjacent to 23
+ * e.g 23 24, 23 25, ..., 23 98, 23 99
+ *
+ */
 void i_print(short int k[2])
 {
 	short int i[2];
@@ -7,15 +18,21 @@ void i_print(short int k[2])
 	i[1] = k[1] + 1; /* **REF** ensure that we always skip current number */
 	while (i[0] <= '9')
 	{
-		while(i[1] <= '9')
+		while (i[1] <= '9')
 		{
 			putchar(k[0]);
 			putchar(k[1]);
 			putchar(' ');
 			putchar(i[0]);
 			putchar(i[1]);
-			putchar(',');
-			putchar(' ');
+			if (!(
+				    (k[0] == '9' && k[1] == '8') &&
+				    (i[0] == '9' && i[1] == '9')
+				    ))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			i[1] = i[1] + 1;
 		}
 		i[0] = i[0] + 1;
@@ -30,9 +47,10 @@ void i_print(short int k[2])
  * main - entry point
  * prints all possible combinations of 2 2-digit numbers
  * 00 - 99
- * Return 0 Always
+ *
+ * Return: 0 Always
  */
-int main()
+int main(void)
 {
 	short int k[2];
 
@@ -40,7 +58,7 @@ int main()
 	while (k[0] <= '9')
 	{
 		k[1] = '0';
-		while(k[1] <= '9')
+		while (k[1] <= '9')
 		{
 			i_print(k);
 			k[1] = k[1] + 1;
