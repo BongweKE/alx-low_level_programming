c8a206b668d61c163459eeb9aad1eb4117cae80a
BongweKE <bongwe.obi@gmail.com>
2022-07-04 01:04:28 +0000

    0x01. C - Variables, if, else, while: 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ * Desctiption: print base16 numbers in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+		c = c + 1;
+	}
+	return (0);
+}
