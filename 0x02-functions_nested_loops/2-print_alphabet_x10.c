aca13c589d418489ac9fee7f3504aa45efa98d9f
BongweKE <bongwe.obi@gmail.com>
2022-07-05 15:48:17 -0700

    0x02. C - Functions, nested loops: 2-print_alphabet_x10.c v0.1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print the whole alphabet
+ *
+ * return: null
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c = c + 1;
+	}
+	_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - print a-z 10times
  *
  * return: null
