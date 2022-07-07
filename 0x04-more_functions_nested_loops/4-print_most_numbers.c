33ac7034a7994b5b276d310fd4bf5b5cb216ea0b
BongweKE <bongwe.obi@gmail.com>
2022-07-07 23:29:03 +0000

    0x04. C - More functions, more nested loops: 4-print_most_numbers.c v 0.1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9 avoid 2,4
+ * print_most_numbers - print 0-9 avoid 2,4
  */
 void print_most_numbers(void)
 {
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 
 	while (c <= '9')
 	{
-		if( !(c == '2' || c == '4') )
+		if (!(c == '2' || c == '4'))
 		{
 			_putchar(c);
 		}
