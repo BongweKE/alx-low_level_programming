9ab199d95598ec5c51bca256112b930126c50e7f
BongweKE <bongwe.obi@gmail.com>
2022-07-07 22:45:30 +0000

    0x04. C - More functions, more nested loops: 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a given input is uppercase
+ * @c: the input to be checked
+ *
+ * Return: 1 or 0 for True or False
+ */
+int _isupper(char c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}
