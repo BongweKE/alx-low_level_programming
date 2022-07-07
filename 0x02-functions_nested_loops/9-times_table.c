271bb7c6f0d38633d1417d3a0ad2ebcdd76c71c4
BongweKE <bongwe.obi@gmail.com>
2022-07-06 18:29:50 -0700

    0x02. C - Functions, nested loops: 9-times_table.c v0.1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * style_if - only add comma and space appropriately
+ * @c: used to check if we're on the first column
+ * @v: used to check if we should add two spaces
+ */
 void style_if(int c, int v)
 {
 	if (c > 0)
@@ -10,8 +15,14 @@ void style_if(int c, int v)
 			_putchar(' ');
 	}
 }
+
+/**
+ * _putlong - used to print out double digit numbers
+ * @v: the double digit number
+ */
 void _putlong(int v)
 {
+	/* last and first digit*/
 	int l, f;
 
 	l = v % 10;
@@ -21,10 +32,15 @@ void _putlong(int v)
 }
 
 /**
- *
+ * times_table - main fxn, used to print a 9x9 times table
+ * Return: nothing
  */
 void times_table(void)
 {
+	/**
+	 * an array a[] created using iterating i
+	 * has rows r and columns c with the current char being v
+	 */
 	int a[10], i, r, c, v;
 
 	/* create array with 0-9 */
@@ -36,13 +52,13 @@ void times_table(void)
 	}
 	/* print out multiples of the array in rows */
 	r = 0;
-	while(r <= 9)
+	while (r <= 9)
 	{
 		c = 0;
 		while (c <= 9)
 		{
 			v = r * a[c];
-			if(v > 9) /** check if it's doube digits **/
+			if (v > 9) /** check if it's doube digits **/
 			{
 				/** only addcomma and space corretly **/
 				style_if(c, v);
