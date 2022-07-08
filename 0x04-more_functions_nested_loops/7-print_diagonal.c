2ad880f49fd4cb735ad481d55e7293b93a07816c
BongweKE <bongwe.obi@gmail.com>
2022-07-08 15:20:08 +0000

    0x04. C - More functions, more nested loops: 7-print_diagonal.c v0.2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,28 +8,31 @@ void print_diagonal(int n)
 {
 	int i, s;
 
-	if (n < 1)
+	if (n > 0)
+	{
+		i = 1;
+		/**
+		 * ensure n = 0,1 will print empty line
+		 * for the rest ensure the spaces before \ are regulated
+		 */
+		while (i <= n)
+		{
+			if (n > 1)
+			{
+				s = 0;
+				while (s < i)
+				{
+					_putchar(' ');
+					s++;
+				}
+				_putchar('\\');
+				_putchar('\n');
+			}
+			i++;
+		}
+	}
+	else
 	{
 		_putchar('\n');
-	}
-	i = 1;
-	/**
-	 * ensure n = 0,1 will print empty line
-	 * for the rest ensure the spaces before \ are regulated
-	 */
-	while (i <= n)
-	{
-		if (n > 1)
-		{
-			s = 0;
-			while (s < i)
-			{
-				_putchar(' ');
-				s++;
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-		i++;
 	}
 }
