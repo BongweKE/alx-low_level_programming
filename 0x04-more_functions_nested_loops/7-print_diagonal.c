02c70de31ac7762fa7dfc3816af5fed29a050d3d
BongweKE <bongwe.obi@gmail.com>
2022-07-08 14:56:55 +0000

    0x04. C - More functions, more nested loops: 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line using the \ symbol
+ * @n: the number of \ to use
+ */
+void print_diagonal(int n)
+{
+	int i, s;
+
+	i = 1;/* ensure n = 0,1 will print empty line */
+	if (n <= 1)
+	{
+		_putchar('\n');
+	}
+	/* for the rest ensure the spaces before \ are regulated */
+	while (i <= n)
+	{
+		if (n > 1)
+		{
+			s = 0;
+			while (s < i)
+			{
+				_putchar(' ');
+				s++;
+			}
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
