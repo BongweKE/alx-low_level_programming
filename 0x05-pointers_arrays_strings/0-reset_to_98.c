c9106bbc78e453e49b48d0a7f1c8461bc825d4dc
BongweKE <bongwe.obi@gmail.com>
2022-07-11 22:03:24 +0000

    0x05. C - Pointers, arrays and strings: 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value pointed to to become 98
+ * @n: The value pointed to which we wanna change to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
