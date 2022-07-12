9c2938e1b99061f96105d5e0caf5c0f24d4e8b06
BongweKE <bongwe.obi@gmail.com>
2022-07-12 00:03:55 +0000

    0x05. C - Pointers, arrays and strings: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - count the length of a string
+ * @s: the string that we want to know the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < CHAR_MAX)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
