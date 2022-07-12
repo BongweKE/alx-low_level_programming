a95d5b345412cc8a648bb6fda2e67b75a421a273
BongweKE <bongwe.obi@gmail.com>
2022-07-12 14:56:08 +0000

    0x05. C - Pointers, arrays and strings: 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - count the length of a string
+ * @s: the string that we want to know the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < INT_MAX)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts2 - prints every other char in a string
+ * @str: the string we will print uniquely
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = _strlen(str);
+	i = 0;
+	while (i < len)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
