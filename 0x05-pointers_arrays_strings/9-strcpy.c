d79504d00ad71c89fc8376da0f8d27d77b4d52c0
BongweKE <bongwe.obi@gmail.com>
2022-07-12 16:10:34 +0000

    0x05. C - Pointers, arrays and strings: 9-strcpy.c v0.1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _strlen - count the length of a string
+ * @s: the string that we want to know the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < INT_MAX)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
+
+
+/**
  * _strcpy - copy a given string to a buffer
  * @src: given string
  * @dest: buffer
@@ -9,11 +31,18 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0, len = _strlen(src);
 
-	while (src[i] != '\0')
+	/* first copy normal chars */
+	while (i < len && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	/* fill the rest with terminator */
+	while (i < len)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
