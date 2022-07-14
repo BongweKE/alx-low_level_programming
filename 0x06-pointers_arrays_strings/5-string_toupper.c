0749ceb80ec15a5f5c3ca2667015622b2bd903fc
BongweKE <bongwe.obi@gmail.com>
2022-07-14 20:27:01 +0000

    0x06. C - More pointers, arrays and strings: 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - count the length of a string
+ * @s: the string that we want to know the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < INT_MAX)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_toupper - convert lowercase chars of a string to uppercase
+ *
+ * Return: the resultant uppercase string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	int d = 'a' - 'A';
+	while (i < _strlen(str))
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - d;
+		}
+		i++;
+	}
+	return (str);
+}
