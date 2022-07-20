8e9ac7f694f0d40dd45bd344e546156688678889
BongweKE <bongwe.obi@gmail.com>
2022-07-20 22:50:43 +0000

    0x08. C - Recursion: 3-factorial.c bettyfied

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
+ * factorial - return the factorial of a number
+ * and if the value is negative return -1 for error
+ * @n: the number we wanna find a factorial of
  *
+ * Return: Factorial of n
  */
 int factorial(int n)
 {
 	if (n == 0 || n == 1)
 	{
-		return(1);
+		return (1);
 	}
-	else if( n < 0)
+	else if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	return (n * factorial(n - 1));
 }
