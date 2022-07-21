dd472cf44564182127365aedf26daae291820fdc
BongweKE <bongwe.obi@gmail.com>
2022-07-21 21:35:19 +0000

    Holders for advanced tasks

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare similarity of two strings
+ * @s1: the first
+ * @s2: second string
+ *
+ * Return: 1 if similar 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (1);
+}
