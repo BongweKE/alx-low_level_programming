9790d0fff2ebd489815f5465d6defba9db41173c
BongweKE <bongwe.obi@gmail.com>
2022-07-28 19:32:50 +0000

    0x0C. C - More malloc, free: 2-calloc.c with segf and null detector

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,23 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *c;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
 	c = malloc(nmemb * size);
 	if (c == NULL)
 	{
 		return (NULL);
+	}
+
+	i = 0;
+	while (i < nmemb)
+	{
+		c[i] = 0;
+		i++;
 	}
 	return (c);
 }
