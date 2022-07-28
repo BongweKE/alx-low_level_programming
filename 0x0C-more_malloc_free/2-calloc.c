04ee9678f7fd134bbee82bb14fc6cb1865c0b549
BongweKE <bongwe.obi@gmail.com>
2022-07-28 19:22:08 +0000

    0x0C. C - More malloc, free: 2-calloc.c bettyfied and with segf detector

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
+ * _calloc - allocate memory to an arrey of given number of elements
+ * each of a given size
+ * @nmemb: number of members
+ * @size: given size of each member
  *
+ * Return: Null or pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *c;
 
 	c = malloc(nmemb * size);
+	if (c == NULL)
+	{
+		return(NULL);
+	}
 	return(c);
 }
