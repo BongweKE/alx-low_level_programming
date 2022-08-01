2e457ab15367b9a1952b62d7f1bceffd62bd2e6e
BongweKE <bongwe.obi@gmail.com>
2022-08-01 21:00:06 +0000

    0x0E. C - Structures, typedef: 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - used to initialize a conceptialization of a dog
+ * @name: the given name
+ * @age: in dog years
+ * @owner: string name of human
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* dog_h */
