f312c9265be4db8b134fb491b823a65e80f3e8e4
BongweKE <bongwe.obi@gmail.com>
2022-08-01 21:17:21 +0000

    0x0E. C - Structures, typedef: 1-init_dog.c v0.1 bettified

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,16 @@
 #include "dog.h"
 
+/**
+ * init_dog - a function to initialize varibles in struct Dog
+ *
+ * @d: pointer to address of resultant struct
+ * @name: jina
+ * @age: in dog years
+ * @owner: human dog friend
+ *
+ * Description: Parse in an address and the details and get a
+ * dog struct as a result
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
