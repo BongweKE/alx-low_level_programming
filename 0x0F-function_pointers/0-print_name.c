d378b26d9ef3b4801fdada48c377daffd871c515
BongweKE <bongwe.obi@gmail.com>
2022-08-03 13:03:14 +0000

    0x0F. C - Function pointers: 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - use function pointer to print a name
+ * in a variety of methods
+ * @name: a string representing the name
+ * @f: a function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
