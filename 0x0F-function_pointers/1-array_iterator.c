082d90d2ad1daf707f05758d018534510bee6c07
BongweKE <bongwe.obi@gmail.com>
2022-08-03 13:19:06 +0000

    0x0F. C - Function pointers: 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function using array members as arguments
+ * during each round of execution
+ * @array: array containing function parameters
+ * @size: size of the array containing parameters
+ * @action: a pointer to the function using array members as parameters
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action == NULL)
+	{
+		/* avoid empty functions */
+		return;
+	}
+	while (i < size)
+	{
+		/* use different argument during each round of execution */
+		action(array[i]);
+		i++;
+	}
+}
