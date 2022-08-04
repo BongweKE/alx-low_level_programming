9e6d7057eaf6cb4bf1649f03f137f85c0d7cb792
BongweKE <bongwe.obi@gmail.com>
2022-08-04 15:32:53 +0000

    0x10. C - Variadic functions: baseline

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif /* variadic_functions_h */
