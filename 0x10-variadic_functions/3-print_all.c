8714b27c3f4eb23e7650e0c0079b20fadef603eb
BongweKE <bongwe.obi@gmail.com>
2022-08-07 23:16:47 +0000

    0x10. C - Variadic functions: v 0.42 collab - modular, stylized, can catch errors if was real printf
    Can catch errors but there's a problem outputing them

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,25 +1,33 @@
 #include "variadic_functions.h"
 /* check for nulls in strings */
 #define NULL_S(S) ((S) == NULL ? ("(nil)") : (S))
+
+/* find errors in specifier == format */
+/* #define NO_MATCH(X, Y) ((X) == (Y) ? ("Hakuna io") : NULL) */
+
 /**
- * print_all - prints anything
- * @format: list of the types of arguments passed
- * if string is null, prints nill
+ * print_args - print given variadic arguments given type specifiers
+ * @args: all the ... arguments
  *
  * Return: nothing
+ * Description:
+ * description will use variables and functions used
+ * First we'll use i to loop through our suspect specifiers in format
+ * j will be used to loop through the real specifiers for each of the
+ * suspect specifiers to know if they are real.
+ * if they are real, we'll use the switch statement to print the ... var
+ * correctly.
+ * if they are not, we can print an error message, or just skip it,
+ * depending on which stage this program is on
  */
-void print_all(const char * const format, ...)
+void print_args(const char * const format, va_list args)
 {
 	size_t i = 0;
 	int j;
 	char *s, *t;
 
 	const char specifiers[] = "cifs";
-/*	const char *type[] = {"char","int","float","string"};
- */
-	va_list args;
 
-	va_start(args, format);
 	while (i  < strlen(format))
 	{
 		j = 0;
@@ -27,34 +35,56 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == specifiers[j])
 			{
-				if (format[i] == 'c')
+				switch (format[i])
 				{
-					printf("%c\n",
-					       (unsigned char) va_arg(args, int));
-				}
-				else if (format[i] == 's')
-				{
+				case 'c':
+					printf("%c",
+					       (unsigned char) va_arg(args,
+								      int));
+					break;
+				case 's':
 					s = va_arg(args, char *);
 					/* NULL_S does every thing */
 					t = NULL_S(s);
-					printf("%s\n", t);
+					printf("%s", t);
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				default:
+					printf("Impossible to happen");
 				}
-				else if (format[i] == 'i')
-				{
-					printf("%d\n", va_arg(args, int));
-				}else if (format[i] == 'f')
-				{
-					printf("%f\n", va_arg(args, double));
-				}
-				else
-				{
-					printf("Print %s\n", "Not available");
-				}
+				/* use to print styler */
+			        (i == (strlen(format) - 1) ?
+				 printf("\n") : printf(", "));
 			}
+			/**
+			 * Here we catch ... which != specifiers
+			 * removed it since it's not very important for now
+			 * t = NO_MATCH(format[i], specifiers[j]);
+			 * ((t) == (NULL) ? printf(" ") : printf("%s\n", t));
+			 */
 			j++;
 		}
 		i++;
 	}
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of the types of arguments passed
+ * if string is null, prints nil
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	print_args(format, args);
 	va_end(args);
-	printf("\n");
 }
