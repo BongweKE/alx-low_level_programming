7c1c2b180367d12bf6ae791e459767b25057f295
BongweKE <bongwe.obi@gmail.com>
2022-08-23 12:41:05 +0000

    0x15. C - File I/O: 0-read_textfile.c Malloc test and close fd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -45,8 +45,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close (fd);
+		return (0);
+	}
 	bytz = read(fd, buf, letters);
 
+	close(fd);
 	if (bytz == -1)
 	{
 		return (0);
