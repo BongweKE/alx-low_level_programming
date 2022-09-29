0070d2e2d80ca147a91d6f5d06432c9d5958e837
BongweKE <bongwe.obi@gmail.com>
2022-09-29 13:37:08 +0000

    Remove unnecessary malloc

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,6 +13,12 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t i;
 
 	current = malloc(sizeof(dlistint_t *));
+	if (current == NULL)
+	{
+		free(current);
+		exit(EXIT_FAILURE);
+	}
+
 	current = (dlistint_t *)h;
 
 	i = 0;
